3612d10a95d4ffd74c3bf3709eaa4e74aa5e88a3
NotSoGoodAtCoding1 <korotkovilya55rus@gmail.com>
2023-05-19 10:13:29 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import math
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    title = "Уравнение"
+    return render_template('index.html', title=title)
+
+
+@app.route('/', methods=['post', 'get'])
+def form():
+    if request.method == 'POST':
+        num_1 = int(request.form.get('num_1'))
+        num_2 = int(request.form.get('num_2'))
+        num_3 = int(request.form.get('num_1'))
+        d = (num_2 * num_2) - 4 * num_1 * num_3
+        if d == 0:
+            ans=-num_2/2*num_1
+            return render_template('index.html', out=ans)
+        elif d<1:
+            return render_template('index.html', out="Дискрименант меньше нуля")
+        else:
+            ans1 = (-num_2 + math.sqrt(d)) / 2 * num_1
+            ans2 = (-num_2 - math.sqrt(d)) / 2 * num_1
+        return render_template('index.html', out=ans1, out2=ans2)
+
+
+if __name__ == '__main__':
+    app.run()
